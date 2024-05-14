388f638fc212591e29ec07903b1ec1e29e54c509
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-05-14 22:13:18 +0600

    writing data with react router

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createFormData,
+} from "./features/order/CreateOrder";
 
 const router = createBrowserRouter([
   {
@@ -22,7 +24,7 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/new", element: <CreateOrder />, action: createFormData },
       {
         path: "/order/:orderId",
         element: <Order />,
