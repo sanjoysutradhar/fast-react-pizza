84b48dad56e2b23e283da08fa6bba4e69070c026
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-05-15 13:13:14 +0600

    styling buttons: Element  states and transitions

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
       <h1 className="mb-8 text-xl font-bold md:text-3xl">
         The best pizza.
         <br />
-        <span className="text-yellow-500">
+        <span className="text-yellow-400">
           Straight out of the oven, straight to you.
         </span>
       </h1>
