6958605135d20dcbe9ac24d3de99c1d0ccd2b73c
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-05-15 16:29:35 +0600

    Configuring Tailwind: Custom font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mone, monospace",
+    },
+    extend: {
+      // colors: {
+      //   pizza: "#123456",
+      // },
+      fontSize: {
+        huge: ["8rem", { lineHeight: "1" }],
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
