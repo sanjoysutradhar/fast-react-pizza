82562723a4ba6e1b0bbdd96d25c8bec3f01fdae6
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-05-16 17:08:31 +0600

    modeling the user state with redux toolkit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,10 @@
+import { useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+
 function Username() {
-  return <div className="hidden text-sm font-bold md:block ">Shanjoy</div>;
+  const username = useSelector((state) => state.user.username);
+  if (!username) return null;
+  return <div className="hidden text-sm font-bold md:block ">{username}</div>;
 }
 
 export default Username;
