c3a5c923d2a0c315ea72c2e08cb917f79713ea23
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-05-16 15:55:08 +0600

    styling the order form

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type }) {
-  const base = `inline-block rounded-full bg-yellow-400  font-bold uppercase 
+  const base = `inline-block text-sm rounded-full bg-yellow-400  font-bold uppercase 
 tracking-wide text-stone-800
  transition-colors
   duration-300 hover:bg-yellow-300
@@ -13,7 +13,7 @@ tracking-wide text-stone-800
     primary: base + "px-4 py-3 md:px-6 md:py-4",
     small: base + "px-4 py-2 md:px-5 md:py-2.5 text-xs",
     secondary: `inline-block rounded-full border-2 border-stone-300 
-     font-bold uppercase 
+     font-bold uppercase  text-sm
     tracking-wide text-stone-400
      transition-colors
       duration-300
