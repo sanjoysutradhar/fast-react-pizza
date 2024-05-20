86f672e556dc043f3927753473f34569dd1d86b3
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-05-20 13:09:33 +0600

    Updating data without navigation

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
 import CreateOrder, {
   action as createFormData,
 } from "./features/order/CreateOrder";
@@ -30,6 +31,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
